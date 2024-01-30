a8167315b8f999d614595871014ca7c5b186d1af
Aryan Singhal <aryansinghal207@gmail.com>
2024-01-30 19:31:29 +0530

    Codes

--- Practicals.c
+++ Practicals.c
@@ -13,17 +13,92 @@
 // }
 
 // ###Write a Program to perform addition, subtraction, division and multiplication of two numbers given as input by the user.
-#include<stdio.h>
-int main(){
-    int a;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    int b;
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    printf("The Sum of a and b is: %d\n",a+b);
-    printf("The difference of a and b is: %d\n",a-b);
-    printf("The divident of a and b is: %d\n",a/b);
-    printf("The multiplication of a and b is: %d",a*b);
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d",&a);
+//     int b;
+//     printf("Enter the value of b:");
+//     scanf("%d",&b);
+//     printf("The Sum of a and b is: %d\n",a+b);
+//     printf("The difference of a and b is: %d\n",a-b);
+//     printf("The divident of a and b is: %d\n",a/b);
+//     printf("The multiplication of a and b is: %d",a*b);
+//     return 0;
+// }
+
+// ###Write a program to evaluate each of the following equations.
+// ###(i) V = u + at. (ii) S = ut+1/2at2 (iii) T=2*a+√b+9c (iv) H=√b2+p2
+
+
+//(i) Solution:
+// #include<stdio.h>
+// int main(){
+//     int u;
+//     printf("Enter the value of u:");
+//     scanf("%d",&u);
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d",&a);
+//     int t;
+//     printf("Enter the value of t:");
+//     scanf("%d",&t);
+//     printf("The Answer after evaluating is V= %f",u+a*t);
+//     return 0;
+// }
+
+
+// (ii) Solution:
+// #include<stdio.h>
+// int main(){
+//     int u;
+//     printf("Enter the value of u:");
+//     scanf("%d",&u);
+//     int t;
+//     printf("Enter the value of t:");
+//     scanf("%d",&t);
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d",&a);
+//     printf("The Answer after evaluating is S= %f",u*t+0.5*a*t*t);
+//     return 0;
+// }    
+
+
+// (iii) Solution:
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int c;
+//     printf("Enter the value of c:");
+//     scanf("%d",&c);
+//     int b;
+//     printf("Enter the value of b:");
+//     scanf("%d",&b);
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d",&a);
+//     printf("The Answer after evaluating is T= %f",2*a+sqrt(b+9*c));
+//     return 0;
+// }    
+
+
+// (iv) Solution:
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int b2;
+//     printf("Enter the value of b2:");
+//     scanf("%d",&b2);
+//     int p2;
+//     printf("Enter the value of p2:");
+//     scanf("%d",&p2);
+//     printf("The Answer after evaluating is H= %f",sqrt(b2+p2));
+//     return 0;
+// }   
+
+
+// ###Write a program to swap two variable:
+// ###(a) By using temporary variable.
+// ###(b) Without using temporary variable 
