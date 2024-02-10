07528cae136c10968d2184820e44ce4111251b92
Aryan <aryansinghal207@gmail.com>
2024-02-10 12:40:40 +0530

    C Patterns Codes

---
+++ Patterns.c
@@ -0,0 +1,34 @@
+// ### Basic pattern code to print any shape using stars by taking input from user... 
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number : ");
+//     scanf("%d",&n);
+//     int m;
+//     printf("Enter a number : ");
+//     scanf("%d",&m);
+//     for (int i = 1; i <=n; i++){
+//         for (int j=1;j< m;j++){
+//             printf("*");
+//         }
+//         printf("*\n");
+//     }
+//     return 0;
+// }
+
+
+// ### A code to print equal number of rows and column in digits...
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter The number of lines : ");
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=a;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
