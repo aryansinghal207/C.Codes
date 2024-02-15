ba6ad463fcf58302892429290dfcf94781652d19
Aryan <aryansinghal207@gmail.com>
2024-02-15 11:13:41 +0530

    C Patterns

--- Patterns.c
+++ Patterns.c
@@ -133,28 +133,48 @@
 // }
 
 
-#include<stdio.h>
+// ###Print the following pattern
+// ***AB
+// ***123
+// ***ABCD
+// ***12345
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("The Number of rows to be printed : ");
+//     scanf("%d",&a);
+//     for(int i=1;i<=a;i++){
+//         if(i%2==0){
+//         int a=1;
+//         for(int j=1;j<=i;j++){
+//             int d=a+64;
+//             char ch=(char)d;
+//             printf("%c",ch);
+//             a++;
+//         }
+//         printf("\n");
+//     }
+//     else{
+//         for(int j=1;j<=i;j++){
+//             printf("%d",j);
+//         }
+//         printf("\n");
+//     }
+//     }
+//     return 0;
+// }
+
+// ###Print the following pattern
+#include<stdio.h> 
 int main(){
     int a;
-    printf("The Number of rows to be printed : ");
+    printf("Enter the value of number of rows : ");
     scanf("%d",&a);
     for(int i=1;i<=a;i++){
-        if(i%2==0){
-        int a=1;
-        for(int j=1;j<=i;j++){
-            int d=a+64;
-            char ch=(char)d;
-            printf("%c",ch);
-            a++;
+        for(int j=1;j<=a;j++){
+            if(j==a/2+1 || i==a/2+1) printf("*");
+            else printf(" ");
         }
         printf("\n");
     }
-    else{
-        for(int j=1;j<=i;j++){
-            printf("%d",j);
-        }
-        printf("\n");
-    }
-    }
-    return 0;
 }
