179ad8dfd781bb0d89032fc4b8b3dc2ff54a6817
Aryan <aryansinghal207@gmail.com>
2024-02-17 21:49:02 +0530

    C Patterns

--- Patterns.c
+++ Patterns.c
@@ -205,7 +205,7 @@ int main(){
     scanf("%d",&a);
     for(int i=1;i<=a;i++){
         for(int j=1;j<=a;j++){
-            if(i==j || i+j==6) printf("*");
+            if(i==j || i+j==a+1) printf("*");
             else printf(" ");
         }
         printf("\n");
