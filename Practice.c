37fc0d9fc509e75eeb1aa47fc6f1185e48aea095
Aryan <aryansinghal207@gmail.com>
2024-02-22 01:06:46 +0530

    C-Codes

---
+++ Practice.c
@@ -0,0 +1,29 @@
+//###Factorial
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter The value for Factorail: ");
+//     scanf("%d",&n);
+//     int product = 1;
+//     int i=1;
+//     for(i=1;i<=n;i++);{
+//         product= product*i;
+//     }
+//     printf("The Factorail is : %d",product);
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    int b;
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    printf("After Swapping the variables a is: %d\n",b);
+    printf("After Swapping the variables b is: %d",a);
+    return 0;
+}
+
+
