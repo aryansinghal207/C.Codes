92e2a1997e13059c5c7a0538870ed83d49ec2b86
Aryan <aryansinghal207@gmail.com>
2024-02-22 23:00:43 +0530

    C

--- Patterns.c
+++ Patterns.c
@@ -231,22 +231,39 @@
 // }
 
 
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter The number of rows :");
+//     scanf("%d",&a);
+//     int x;
+//     for(int i=1;i<=a;i++){
+//         if(i%2!=0) x=1; 
+//         else x=0;
+//         for(int j=1;j<=i;j++){
+//             printf("%d",x);
+//             if(x==0) x=1;
+//             else x=0;
+//         }
+//         printf("\n");
+        
+//     }
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
-    int a;
-    printf("Enter The number of rows :");
-    scanf("%d",&a);
     int x;
-    for(int i=1;i<=a;i++){
-        if(i%2!=0) x=1; 
-        else x=0;
-        for(int j=1;j<=i;j++){
-            printf("%d",x);
-            if(x==0) x=1;
-            else x=0;
+    printf("Enter the number of rows : ");
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++){
+        for(int j=1;j<=x-i;j++){
+            printf("#");
+        }
+        for(int k=1;k<=i;k++){
+            printf("*");
         }
         printf("\n");
-        
     }
-    return 0;
 }
