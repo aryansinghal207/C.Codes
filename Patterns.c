172b5fc2ddd55c7602efad4bdd5abfac90a9ca1d
Aryan <aryansinghal207@gmail.com>
2024-02-22 22:20:32 +0530

    C-Codes

--- Patterns.c
+++ Patterns.c
@@ -198,16 +198,55 @@
 // }
 
 // ### Star Cross pattern
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter The number of lines : ");
+//     scanf("%d",&a);
+//     for(int i=1;i<=a;i++){
+//         for(int j=1;j<=a;j++){
+//             if(i==j || i+j==a+1) printf("*");
+//             else printf(" ");
+//         }
+//         printf("\n");
+//     }  
+// }
+
+// ###Floyd's Triangle
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter The number of rows :");
+//     scanf("%d",&a);
+//     int x=1;
+//     for(int i=1;i<=a;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("%d",x);
+//             x++;
+//         }
+//         printf("\n");
+        
+//     }
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
     int a;
-    printf("Enter The number of lines : ");
+    printf("Enter The number of rows :");
     scanf("%d",&a);
+    int x;
     for(int i=1;i<=a;i++){
-        for(int j=1;j<=a;j++){
-            if(i==j || i+j==a+1) printf("*");
-            else printf(" ");
+        if(i%2!=0) x=1; 
+        else x=0;
+        for(int j=1;j<=i;j++){
+            printf("%d",x);
+            if(x==0) x=1;
+            else x=0;
         }
         printf("\n");
-    }  
+        
+    }
+    return 0;
 }
