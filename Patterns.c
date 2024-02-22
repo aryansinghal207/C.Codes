8080c344a0a6a71b86867e4bf64ac0ff8e1fafce
Aryan <aryansinghal207@gmail.com>
2024-02-22 23:02:56 +0530

    C

--- Patterns.c
+++ Patterns.c
@@ -231,6 +231,7 @@
 // }
 
 
+// ### 0&1 Triangle###
 // #include<stdio.h>
 // int main(){
 //     int a;
@@ -252,6 +253,25 @@
 // }
 
 
+
+// ###Star Triangle MAST###
+// #include<stdio.h>
+// int main(){
+//     int x;
+//     printf("Enter the number of rows : ");
+//     scanf("%d",&x);
+//     for(int i=1;i<=x;i++){
+//         for(int j=1;j<=x-i;j++){
+//             printf(" ");
+//         }
+//         for(int k=1;k<=i;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+
 #include<stdio.h>
 int main(){
     int x;
@@ -259,9 +279,9 @@ int main(){
     scanf("%d",&x);
     for(int i=1;i<=x;i++){
         for(int j=1;j<=x-i;j++){
-            printf("#");
+            printf(" ");
         }
-        for(int k=1;k<=i;k++){
+        for(int k=1;k<=4;k++){
             printf("*");
         }
         printf("\n");
