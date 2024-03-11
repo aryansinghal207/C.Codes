4d7cf9f9823d1eea7b8cafffc56866414217e80d
Aryan <aryansinghal207@gmail.com>
2024-03-11 22:30:33 +0530

    C-Codes

--- functions.c
+++ functions.c
@@ -88,28 +88,53 @@
 
 
 // Calling Function in function without any proper sequence(Function Prototype)
-#include <stdio.h>
+// #include <stdio.h>
 
-void australia(){
-    printf("You are in Australia \n");
-    void england();
-    england();
-    return;
+// void australia(){
+//     printf("You are in Australia \n");
+//     void england();
+//     england();
+//     return;
+// }
+// int main() {
+//     void india();
+//     india();
+//     return 0;
+// }
+
+// void india(){
+//     printf("You are in India \n");
+//     void australaia();
+//     australia();
+//     return;
+// }
+
+// void england(){
+//     printf("You are in England \n");
+//     return;
+// }
+
+
+//Combination Programme
+#include<stdio.h>
+int factorial(int x){
+    int fact =1;
+    for(int i=2;i<=x;i++){
+        fact=fact*i;
+    }
+    return fact;
 }
-int main() {
-    void india();
-    india();
+int main(){
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    int r;
+    printf("Enter the value of r : ");
+    scanf("%d",&r);
+    int nfact=factorial(n);
+    int rfact=factorial(r);
+    int nrfact=factorial(n-r);
+    int ncr=nfact/(rfact*nrfact);
+    printf("%d",ncr);
     return 0;
-}
-
-void india(){
-    printf("You are in India \n");
-    void australaia();
-    australia();
-    return;
-}
-
-void england(){
-    printf("You are in England \n");
-    return;
 }
