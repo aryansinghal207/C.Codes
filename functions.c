0a298e31a723ee69cf3b91aa67be85415b5db88b
Aryan <aryansinghal207@gmail.com>
2024-03-11 22:32:12 +0530

    C-Codes

--- functions.c
+++ functions.c
@@ -141,6 +141,22 @@
 
 
 //Factorial Programme
+// #include<stdio.h>
+// int factorial(int x){
+//     int fact =1;
+//     for(int i=2;i<=x;i++){
+//         fact=fact*i;
+//     }
+//     return fact;
+// }
+// int main(){
+//     int a=factorial(6);
+//     printf("%d",a);
+//     return 0;
+// }
+
+
+//Pascal Triangle in triangular form
 #include<stdio.h>
 int factorial(int x){
     int fact =1;
@@ -149,8 +165,20 @@ int factorial(int x){
     }
     return fact;
 }
+int combination(int n,int r){
+    int ncr=factorial(n)/(factorial(r)*factorial(n-r));
+    return ncr;
+}
 int main(){
-    int a=factorial(6);
-    printf("%d",a);
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            int icj=combination(i,j);
+            printf("%d ",icj);
+        }
+        printf("\n");
+    }
     return 0;
 }
