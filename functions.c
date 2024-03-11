03ced67dabd58810ab9c30734b8c3232788bd8f2
Aryan <aryansinghal207@gmail.com>
2024-03-11 22:26:45 +0530

    C-Codes

--- functions.c
+++ functions.c
@@ -25,22 +25,40 @@
 // }
 
 // Calling Function in function
+// #include <stdio.h>
+// void england(){
+//     printf("You are in England \n");
+//     return;
+// }
+// void australia(){
+//     printf("You are in Australia \n");
+//     england();
+//     return;
+// }
+// void india(){
+//     printf("You are in India \n");
+//     australia();
+//     return;
+// }
+// int main() {
+//     india();
+//     return 0;
+// }
+
+
+// Sum of 2 numbers using functions
 #include <stdio.h>
-void england(){
-    printf("You are in England \n");
-    return;
-}
-void australia(){
-    printf("You are in Australia \n");
-    england();
-    return;
-}
-void india(){
-    printf("You are in India \n");
-    australia();
+void sum(){
+    int a,b;
+    printf("Enter First Number : ");
+    scanf("%d",&a);
+    printf("Enter Second Number : ");
+    scanf("%d",&b);
+    printf("The Sum of Entered Number is : %d \n",a+b);
     return;
 }
 int main() {
-    india();
+    sum();
+    sum();
     return 0;
 }
