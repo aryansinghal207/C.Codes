28772cf8c236bdb86d051495f83b02d338f17e52
Aryan <aryansinghal207@gmail.com>
2024-03-11 22:31:23 +0530

    C-Codes

--- functions.c
+++ functions.c
@@ -116,6 +116,31 @@
 
 
 //Combination Programme
+// #include<stdio.h>
+// int factorial(int x){
+//     int fact =1;
+//     for(int i=2;i<=x;i++){
+//         fact=fact*i;
+//     }
+//     return fact;
+// }
+// int main(){
+//     int n;
+//     printf("Enter the value of n : ");
+//     scanf("%d",&n);
+//     int r;
+//     printf("Enter the value of r : ");
+//     scanf("%d",&r);
+//     int nfact=factorial(n);
+//     int rfact=factorial(r);
+//     int nrfact=factorial(n-r);
+//     int ncr=nfact/(rfact*nrfact);
+//     printf("%d",ncr);
+//     return 0;
+// }
+
+
+//Factorial Programme
 #include<stdio.h>
 int factorial(int x){
     int fact =1;
@@ -125,16 +150,7 @@ int factorial(int x){
     return fact;
 }
 int main(){
-    int n;
-    printf("Enter the value of n : ");
-    scanf("%d",&n);
-    int r;
-    printf("Enter the value of r : ");
-    scanf("%d",&r);
-    int nfact=factorial(n);
-    int rfact=factorial(r);
-    int nrfact=factorial(n-r);
-    int ncr=nfact/(rfact*nrfact);
-    printf("%d",ncr);
+    int a=factorial(6);
+    printf("%d",a);
     return 0;
 }
