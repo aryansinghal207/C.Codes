5e21d04bfeb0dcd80062ed5223ba068e9e7846dd
Aryan <aryansinghal207@gmail.com>
2024-03-28 22:05:40 +0530

    C Codes

--- Pointeres.c
+++ Pointeres.c
@@ -1,7 +1,15 @@
+// #include<stdio.h>
+// int main(){
+//     int a=5;
+//     int* p=&a;// This will store the address of a.
+//     printf("%p",p);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
     int a=5;
-    int* p=&a;
-    printf("%p",p);
+    int* p=&a;// This will store the address of a.
+    printf("%d",*p);
     return 0;
 }
