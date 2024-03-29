eee35a30f1ae42d66f7b25c8815f6ec70082c3b9
Aryan <aryansinghal207@gmail.com>
2024-03-29 09:28:09 +0530

    c codes

--- Practicals.c
+++ Practicals.c
@@ -103,15 +103,4 @@
 // ###(a) WIthout using third variable
 // ###(b) By using temporary variable.
 // ###(c) Without using temporary variable 
-#include<stdio.h>
-int main(){
-    int a;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    int b;
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    printf("After Swapping the variables a is: %d\n",b);
-    printf("After Swapping the variables b is: %d",a);
-    return 0;
-}
+
