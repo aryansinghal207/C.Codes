169f3bd4860185b90da2ae79da918f9874d77881
Aryan <aryansinghal207@gmail.com>
2024-04-08 22:32:04 +0530

    c codes

---
+++ Array.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={2,3,4,5,6};
+    arr[2]=9;
+    printf("%d",arr[2]);                                                                   
+    return 0;
+}
