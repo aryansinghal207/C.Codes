e7069de3343e85771cf6b1acd48dbac2119905e4
Aryan <aryansinghal207@gmail.com>
2024-04-10 01:51:03 +0530

    c codes

--- Array.c
+++ Array.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
-    int arr[5]={2,3,4,5,6};
-    arr[2]=9;
+    int arr[4];
+    for(int i=0;i<=4;i++){
+        scanf("%d ",&arr[i]);
+    }
     printf("%d",arr[2]);                                                                   
     return 0;
 }
