9a98ee00cb4dfcf8f033a1d00a6742e8cd9e1660
Aryan <aryansinghal207@gmail.com>
2024-04-10 02:16:01 +0530

    c codes

--- Array.c
+++ Array.c
@@ -8,12 +8,23 @@
 //     return 0;
 // } 
 
+// #include<stdio.h>
+// int main(){
+//     int marks[10]={11,22,33,44,55,66,77,88,99,00};
+//     for(int i=0;i<=9;i++){
+//         if(marks[i]<=35){
+//             printf("%d \n",marks[i]);
+//         }
+//     }                                                                 
+//     return 0;
+// } 
+
 #include<stdio.h>
 int main(){
     int marks[10]={11,22,33,44,55,66,77,88,99,00};
     for(int i=0;i<=9;i++){
         if(marks[i]<=35){
-            printf("%d \n",marks[i]);
+            printf("%d \n",i);
         }
     }                                                                 
     return 0;
