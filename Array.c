f9584f19d54bd2e9242f04039badcaa6c995b012
Aryan <aryansinghal207@gmail.com>
2024-04-11 21:07:53 +0530

    c codes

--- Array.c
+++ Array.c
@@ -19,13 +19,24 @@
 //     return 0;
 // } 
 
+// #include<stdio.h>
+// int main(){
+//     int marks[10]={11,22,33,44,55,66,77,88,99,00};
+//     for(int i=0;i<=9;i++){
+//         if(marks[i]<=35){
+//             printf("%d \n",i);
+//         }
+//     }                                                                 
+//     return 0;
+// } 
+
 #include<stdio.h>
 int main(){
-    int marks[10]={11,22,33,44,55,66,77,88,99,00};
-    for(int i=0;i<=9;i++){
-        if(marks[i]<=35){
-            printf("%d \n",i);
-        }
-    }                                                                 
+    int num[5]={1,2,3,45,5};
+    int sum=0;
+    for(int i;i<=4;i++){
+        sum=sum + num[i];
+    }
+    printf("%d",sum);
     return 0;
-} 
+}
