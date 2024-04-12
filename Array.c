1c4dade7b394a6d4f13a4f48afc41b8958c50150
Aryan <aryansinghal207@gmail.com>
2024-04-12 18:40:11 +0530

    c codes

--- Array.c
+++ Array.c
@@ -41,13 +41,25 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int main(){
+//     int num[5]={1,2,3,4,5};
+//     int sum=1;
+//     for(int i;i<=4;i++){
+//         sum=sum*num[i];
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int num[5]={1,2,3,4,5};
-    int sum=1;
-    for(int i;i<=4;i++){
-        sum=sum*num[i];
+    int arr[5]={1,2,3,4,5,9};
+    int a=-1;
+    for(int i=0;i<=5;i++){
+        if(a<arr[i]){
+            i=arr[i];
+        }
+        printf("%d",a);
     }
-    printf("%d",sum);
-    return 0;
 }
--- Practice.c
+++ Practice.c
@@ -13,16 +13,54 @@
 //     return 0;
 // }
 
-#include<stdio.h>
-int main(){
-    int a;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    int b;
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    printf("After Swapping the variables a is: %d\n",b);
-    printf("After Swapping the variables b is: %d",a);
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter the value of a:");
+//     scanf("%d",&a);
+//     int b;
+//     printf("Enter the value of b:");
+//     scanf("%d",&b);
+//     printf("After Swapping the variables a is: %d\n",b);
+//     printf("After Swapping the variables b is: %d",a);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// struct Student {
+//     char name[50];
+//     int rollno;
+//     float marks;
+// };
+// int main() {
+//     struct Student Student1;
+//     printf("Enter name: ");
+//     scanf("%s", Student1.name);
+//     printf("Enter Roll No: ");
+//     scanf("%d", &Student1.rollno);
+//     printf("Enter Marks: ");
+//     scanf("%f", &Student1.marks);
+//     printf("\nDetails of Student1:\n");
+//     printf("Name: %s\n", Student1.name);
+//     printf("Roll No: %d\n", Student1.rollno);
+//     printf("Marks: %.2f\n", Student1.marks);
+//     return 0;
+// }
+
+#include <stdio.h>
+struct height {
+    int feet;
+    float inches;
+};
+int main() {
+    struct height height1;
+    printf("Enter height in feets : ");
+    scanf("%d", &height1.feet);
+    printf("Enter height in inches: ");
+    scanf("%f", &height1.inches);
+    printf("\nHeight of individual:\n");
+    printf("The Height is %d feet and %.1f inches",height1.feet,height1.inches);
     return 0;
 }
 
