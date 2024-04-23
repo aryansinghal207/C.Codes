41b6509f44cfa1776ff6b578b66c00d4b5596af5
Aryan <aryansinghal207@gmail.com>
2024-04-23 23:53:11 +0530

    c codes

--- 2DArray.c
+++ 2DArray.c
@@ -11,24 +11,16 @@
 //     return 0;
 // }
 
-// Write a 2D array in C to store the elements of a matrix of size 3x3 by taking input from user and print them.
+// ###Find the sum of all the elements of a 2D array in C.
 #include<stdio.h>
 int main(){
-    int a,b;
-    int arr[a][b];
-    printf("Enter the number of rows and columns: ");
-    scanf("%d %d",&a,&b);
-    for(int i=0;i<a;i++){
-        for(int j=0;j<b;b++){
-            printf("Enter the element of %d row and %d columns:",i,j);
-            scanf("%d",&arr[i][j]);
+    int arr[2][3]={{1,2,3},{4,5,6}};
+    int sum=0;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            sum+=arr[i][j];
         }
     }
-    for(int i=0;i<a;i++){
-        for(int j=0;j<b;j++){
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
-    }
+    printf("%d",sum);
     return 0;
-}   
+}
