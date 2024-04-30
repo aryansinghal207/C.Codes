51f7f050913f9a43f0c00e5e9366515b0bf8c008
Aryan <aryansinghal207@gmail.com>
2024-05-01 01:42:03 +0530

    c codes

--- strings.c
+++ strings.c
@@ -9,11 +9,22 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int main(){
+//     char arr[]="Aryan Singhal";
+//     int i=0;
+//     while(i<13){
+//         printf("%c",arr[i]);
+//         i++;
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    char arr[]="Aryan Singhal";
+    char arr[]="Aryan Singhal is the best \0";
     int i=0;
-    while(i<13){
+    while(arr[i]!='\0'){
         printf("%c",arr[i]);
         i++;
     }
