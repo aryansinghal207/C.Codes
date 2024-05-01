61cd0c40ecd22a616af76f15f5a2936d4a227d7a
Aryan <aryansinghal207@gmail.com>
2024-05-01 15:58:33 +0530

    c codes

--- strings.c
+++ strings.c
@@ -31,14 +31,22 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int main(){
+//     char arr[]="Aryan Singhal is the best \0";
+//     arr[1]='R';
+//     int i=0;
+//     while(arr[i]!='\0'){
+//         printf("%c",arr[i]);
+//         i++;
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
+#include<string.h>
 int main(){
-    char arr[]="Aryan Singhal is the best \0";
-    arr[1]='R';
-    int i=0;
-    while(arr[i]!='\0'){
-        printf("%c",arr[i]);
-        i++;
-    }
+    char str[]="Aryan Singhal is the best";
+    puts(str);
     return 0;
 }
