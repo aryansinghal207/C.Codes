4464b46ae48745c56b26c2b3d7a15131e36c25a5
Aryan <aryansinghal207@gmail.com>
2024-05-01 16:10:19 +0530

    c codes

--- strings.c
+++ strings.c
@@ -43,10 +43,19 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char str[]="Aryan Singhal is the best";
+//     puts(str);
+//     return 0;
+// }
+
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[]="Aryan Singhal is the best";
-    puts(str);
+    char str[40];
+    gets(str);
+    printf("Your input was : %s",str);
     return 0;
 }
