7deb822f4130b4d5fabc31e5181d668701505a8f
Aryan <aryansinghal207@gmail.com>
2024-05-04 10:01:01 +0530

    c codes

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main(){
+    char str[40]="Aryan";
+    printf("%s \n",str);
+    for(int i=6;i>=2;i--){
+        str[i+1]=str[i];
+    }
+    str[2]='k';
+    printf("%s",str);
+    return 0;
+}
