98f71c541dc4165c35eaa3f2d44afba204253311
Aryan <aryansinghal207@gmail.com>
2024-05-05 22:02:40 +0530

    g

--- recursion.c
+++ recursion.c
@@ -33,17 +33,34 @@
 
 
 // WAP to print from 1 to n
+// #include<stdio.h>
+// void inc(int x,int n){
+//     if(x>n) return;
+//     printf("%d\n",x);
+//     inc(x+1,n);
+//     return;
+// }
+// int main(){
+//     int n;
+//     printf("Enter the numbers of u want to printb the number : ");
+//     scanf("%d",&n);
+//     inc(1,n);
+//     return 0;
+// }
+
+
+// WAP to print a raised to power b using recursion..
 #include<stdio.h>
-void inc(int x,int n){
-    if(x>n) return;
-    printf("%d\n",x);
-    inc(x+1,n);
-    return;
+int power(int a,int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
 }
 int main(){
-    int n;
-    printf("Enter the numbers of u want to printb the number : ");
-    scanf("%d",&n);
-    inc(1,n);
+    int a,b;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+    printf("Enter the power : ");
+    scanf("%d",&b);
+    printf("The result is %d",power(a,b));
     return 0;
 }
