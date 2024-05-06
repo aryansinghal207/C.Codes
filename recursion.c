e17c6a4d2f5b1c12d7c7710917c739f380f8a4d6
Aryan <aryansinghal207@gmail.com>
2024-05-06 11:00:10 +0530

    g

--- recursion.c
+++ recursion.c
@@ -50,17 +50,34 @@
 
 
 // WAP to print a raised to power b using recursion..
+// #include<stdio.h>
+// int power(int a,int b){
+//     if(b==0) return 1;
+//     return a*power(a,b-1);
+// }
+// int main(){
+//     int a,b;
+//     printf("Enter the number : ");
+//     scanf("%d",&a);
+//     printf("Enter the power : ");
+//     scanf("%d",&b);
+//     printf("The result is %d",power(a,b));
+//     return 0;
+// }
+
+// WAP to calculate factorail using recursion
 #include<stdio.h>
-int power(int a,int b){
-    if(b==0) return 1;
-    return a*power(a,b-1);
+int fibo(int n){
+    if(n==0 || n==1 || n==2) return 1;
+    int a1=fibo(n-1);
+    int a2=fibo(n-2);
+    return a1+a2;
 }
 int main(){
-    int a,b;
+    int n;
     printf("Enter the number : ");
-    scanf("%d",&a);
-    printf("Enter the power : ");
-    scanf("%d",&b);
-    printf("The result is %d",power(a,b));
+    scanf("%d",&n);
+    int x=fibo(n);
+    printf("The Factorial of %d is %d",n,x);
     return 0;
 }
