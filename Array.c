7f387af0d53e8b790f618b966c07fa15b9319eba
Aryan Singhal <157485834+aryansinghal207@users.noreply.github.com>
2024-05-23 23:45:06 +0530

    Update Array.c

--- Array.c
+++ Array.c
@@ -1,16 +1,5 @@
 // #include<stdio.h>
 // int main(){
-//     int arr[4];
-//     for(int i=0;i<=4;i++){
-//         scanf("%d ",&arr[i]);
-//     }
-//     printf("%d",arr[2]);                                                                   
-//     return 0;
-// } 
-
-
-// #include<stdio.h>
-// int main(){
 //     int marks[10]={11,22,33,44,55,66,77,88,99,00};
 //     for(int i=0;i<=9;i++){
 //         if(marks[i]<=35){
@@ -19,6 +8,7 @@
 //     }                                                                 
 //     return 0;
 // } 
+
 
 // #include<stdio.h>
 // int main(){
